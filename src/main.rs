60c666398d58ace0fb3a190744008955f2dc1008
Spencer Smith <spencer.smith@toyotaconnected.com>
2024-12-30 14:50:32 -0600

    fix formatting

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,8 @@ async fn health_check() -> impl Responder {
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
