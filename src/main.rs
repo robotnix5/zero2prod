bcc437840eb20fbd731e0df7dbe970c55d8d1d79
Spencer Smith <spencer.smith@toyotaconnected.com>
2025-01-20 15:19:26 -0600

    refactor health endpoint to match the book

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+    HttpServer::new(|| { App::new()
+        .route("/", web::get().to(greet))
+        .route("/{name}", web::get().to(greet))})
         .bind("127.0.0.1:8000")?
         .run()
         .await
