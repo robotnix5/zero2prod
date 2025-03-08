67ee6c93934f3de26ce1fab7546c26541cbf7dcb
Spencer Smith <spencer.smith@toyotaconnected.com>
2025-03-08 16:01:04 -0600

    rough configuration

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 //! main.rs
 
+use zero2prod::configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener =
-        std::net::TcpListener::bind("127.0.0.1:0").expect("failed to bind to random port");
-    println!("listener: {:?}", listener);
+    let configuration = configuration::get_configuration().expect("failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = std::net::TcpListener::bind(address)?;
     run(listener)?.await
 }
